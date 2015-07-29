9f9304c00ce094bae1be3bf96ce1e853b7f51ddc
paul <psoucy@sciaps.com>
2015-07-29 17:05:56 -0400

    started migration to meson

--- include/microdb.h
+++ include/microdb.h
@@ -4,20 +4,20 @@
 
 #include <stdio.h>
 
-namespace microdb{
-    
+namespace microdb {
+
     static const int kMajorVersion = 1;
     static const int kMinorVersion = 0;
-    
+
     class DB {
     public:
         static Status Open(const std::string& dbdirpath, DB** dbptr);
-        
+
         virtual ~DB();
-        
+
         virtual Status AddView(const std::string& viewName, const std::string& mapQuery) = 0;
         virtual Status DeleteView(const std::string& viewName) = 0;
-        
+
         virtual Status Insert(const std::string& value, std::string* key = nullptr) = 0;
         virtual Status Update(const std::string& key, const std::string& value) = 0;
         virtual Status Delete(const std::string& key) = 0;
