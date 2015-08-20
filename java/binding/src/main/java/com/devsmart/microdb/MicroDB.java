98276362e1947e1b9d0af0e84c1195d552e0c7d9
Paul Soucy <paul@dev-smart.com>
2015-08-20 01:23:23 -0400

    wip

--- java/binding/src/main/java/com/devsmart/microdb/MicroDB.java
+++ java/binding/src/main/java/com/devsmart/microdb/MicroDB.java
@@ -1,20 +1,47 @@
 package com.devsmart.microdb;
 
 
+import com.devsmart.microdb.ubjson.UBObject;
 import com.devsmart.microdb.ubjson.UBValue;
+
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
 
 public class MicroDB {
 
+    private HashMap<UBValue, SoftReference<DBObject>> mLiveObjects = new HashMap<>()
+
+
+    public <T extends DBObject> T create(Class<T> classType) {
+        try {
+            if(!classType.getSimpleName().endsWith("_pxy")) {
+                String proxyClassName = String.format("%s.%s_pxy", DBObject.class.getPackage().getName(), classType.getSimpleName());
+                classType = (Class<T>) Class.forName(proxyClassName);
+            }
+
+            T retval = classType.newInstance();
+
+            //TODO: init data
+            UBObject data = null;
+            retval.init(data, this);
+            return retval;
+        } catch (Exception e) {
+            throw new RuntimeException("", e);
+        }
+    }
 
     public <T extends DBObject> T get(UBValue id, Class<T> classType) {
-        return null;
+        try {
+            //TODO: look in the cache / load the data
+            UBObject data = null;
+            T retval = classType.newInstance();
+            retval.init(data, this);
+            return retval;
+        } catch (Exception e){
+            throw new RuntimeException("", e);
+        }
     }
 
-    public String insert(DBObject obj) {
-        //TODO:
-        return null;
-
-    }
 
     public void update(DBObject obj) {
 
