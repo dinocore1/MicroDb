0825c1ea4261d90fc175d6f6fb82804d0a8d11c0
Paul Soucy <paul@dev-smart.com>
2016-01-23 13:34:12 -0500

    working

--- microdb-runtime/src/test/java/com/devsmart/microdb/ReadWriteTest.java
+++ microdb-runtime/src/test/java/com/devsmart/microdb/ReadWriteTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class ReadWriteTest {
 
     @Test
-    public void testReadWrite() throws Exception {
+    public void testWriteEmbeddedObj() throws Exception {
 
         MicroDB db = DBBuilder.builder()
                 .buildMemoryDB();
@@ -33,5 +33,29 @@ public class ReadWriteTest {
         assertNotNull(dobj1.get("myDBO"));
         assertTrue(dobj1.get("myDBO").isObject());
 
+    }
+
+    @Test
+    public void testWriteIDRefObj() throws Exception {
+        MicroDB db = DBBuilder.builder()
+                .buildMemoryDB();
+
+        MyDBObj obj1 = db.insert(MyDBObj.class);
+        obj1.setMyString("obj1");
+
+        MyDBObj obj2 = db.insert(MyDBObj.class);
+        obj2.setMyString("obj2");
+
+        obj1.setMyDBO(obj2);
+
+        UBObject data = UBValueFactory.createObject();
+        obj1.writeToUBObject(data);
+
+        assertNotNull(data);
+        assertTrue(data.isObject());
+        UBObject dobj1 = data.asObject();
+        assertNotNull(dobj1.get("id"));
+        assertNotNull(dobj1.get("myDBO"));
+        assertTrue(dobj1.get("myDBO").isString());
     }
 }
