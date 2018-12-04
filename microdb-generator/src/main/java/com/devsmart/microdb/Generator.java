ef0d0cdb2a95e6d42bb694a40fd5159413b5109e
Stephen Gowen <dev.sgowen@gmail.com>
2018-12-04 11:31:18 -0500

    Fixing Windows build error... I think.

--- microdb-generator/src/main/java/com/devsmart/microdb/Generator.java
+++ microdb-generator/src/main/java/com/devsmart/microdb/Generator.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.LinkedHashSet;
+import java.util.regex.Matcher;
 
 public class Generator {
 
@@ -32,7 +33,7 @@ public class Generator {
             JavaCodeGenerator generator = new JavaCodeGenerator(dbo, mFileNode);
 
             String code = generator.generateCode();
-            final String output = mFileNode.packageName.replaceAll("\\.", File.separator);
+            final String output = mFileNode.packageName.replaceAll("\\.", Matcher.quoteReplacement(File.separator));
             File outputDir = new File(mOutputDir, output);
             outputDir.mkdirs();
             File outputFile = new File(outputDir, dbo.name + ".java");
